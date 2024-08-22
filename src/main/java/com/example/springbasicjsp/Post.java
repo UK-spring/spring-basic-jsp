72750a061fd840091727bff62bd4793492b222ed
UK <hwangwonuk94@gmail.com>
2024-08-23 07:06:34 +0900

    fix: 사용하지 않는 Lombok 제거

--- src/main/java/com/example/springbasicjsp/Post.java
+++ src/main/java/com/example/springbasicjsp/Post.java
@@ -1,12 +1,10 @@
 package com.example.springbasicjsp;
 
-import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.Setter;
 
 @Getter
 @Setter
-@AllArgsConstructor
 public class Post {
 
     private Long id;
